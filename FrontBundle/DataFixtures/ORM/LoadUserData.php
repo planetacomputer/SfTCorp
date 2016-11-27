66fc996c74ce9bf62e8eaf2fbbe3b2eaf4db457d
gafilter <gafilter@gmail.com>
2016-11-27 19:25:09 +0100

    autenticacion con FOSUserBundle y modificacion juego de pruebas

--- FrontBundle/DataFixtures/ORM/LoadUserData.php
+++ FrontBundle/DataFixtures/ORM/LoadUserData.php
@@ -2,25 +2,51 @@
 namespace TechCorp\FrontBundle\DataFixtures\ORM;
 
 use Doctrine\Common\DataFixtures\AbstractFixture;
-//use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
-
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use TechCorp\FrontBundle\Entity\User;
 
-class LoadUserData extends AbstractFixture implements OrderedFixtureInterface{
+class LoadUserData extends AbstractFixture implements FixtureInterface, OrderedFixtureInterface, ContainerAwareInterface{
 	const MAX_NB_USERS = 10;
+
+	/**
+	* @var ContainerInterface
+	*/
+	private $container;
+	
+	/**
+	* {@inheritDoc}
+	*/
+	public function setContainer(ContainerInterface $container = null){
+		$this->container = $container;
+	}
+
+
 	public function load(ObjectManager $manager){
 		$faker = \Faker\Factory::create();
+		$userManager = $this->container->get("fos_user.user_manager");
 
 		for($i = 0; $i < self::MAX_NB_USERS; ++$i){
-			$user = new User();
+			$user = $userManager->createUser();
 			$user->setUsername($faker->username);
 			$user->setEmail($faker->email);
-			$user->setPassword($faker->password);
-			$manager->persist($user);
+			$user->setPlainPassword($faker->password);
+			$user->setEnabled(true);
 			$this->addReference('user'.$i, $user);
+			
+			$manager->persist($user);
 		}
+
+		$user = $userManager->createUser();
+		$user->setUsername('user');
+		$user->setEmail($faker->email);
+		$user->setPlainPassword('password');
+		$user->setEnabled(true);
+
+		$manager->persist($user);
 
 		$manager->flush();
 	}
@@ -28,4 +54,5 @@ class LoadUserData extends AbstractFixture implements OrderedFixtureInterface{
 	public function getOrder(){
 		return 1;
 	}
+
 }
